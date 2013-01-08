6a081025838fe32a885d5804bb4912fc5775dbb6
David Souther <davidsouther@gmail.com>
2013-01-07 19:32:20 -0500

    Moved to Superscore 0.3.3 and recompiled using global event$ symbols.

--- grunt.js
+++ grunt.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
 			banner: '// <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
 				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
 				'<%= pkg.homepage ? "// " + pkg.homepage + "\n" : "" %>' +
-				'// Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+				'// Copyright (c) 2012 - <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
 				' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>'
 		},
 		clean: {
@@ -21,7 +21,8 @@ module.exports = function(grunt) {
 					"dist/compiled/Transaction.js": 'src/Transaction.ls',
 				},
 				options: {
-					bare: true
+					bare: true,
+					util: true
 				}
 			},
 			nunit: {
